5fbc6ff329d50309804cca77bcfa67565363a236
V0idC0de <26016825+V0idC0de@users.noreply.github.com>
2025-08-18 17:15:48 +0200

    feat: Add pre-commit-hooks lab

---
+++ labs/00-pre-commit-hooks/lab-repository/sample.py
@@ -0,0 +1,11 @@
+import os
+
+def insecure_function():
+    key = "AKIALALEMEL33243OLIA"  # Hardcoded secret for gitleaks
+    print(  
+              'Hello, world!' ) # Formatting error for black
+    print("This is a test")
+    return 42
+
+
+
