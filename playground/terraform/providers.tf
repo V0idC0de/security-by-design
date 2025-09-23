a181a6f0582893b21409f500258149350da1f430
V0idC0de <--help>
2025-09-24 01:13:45 +0200

    playground: Adding initial version of the code and docs

---
+++ playground/terraform/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.11"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
+  }
+}
+
+provider "tls" {}
+
+provider "google" {
+  region = var.region
+}
