f15167db8050fa3fe49074c07cba97dc70f370f4
V0idC0de <26016825+V0idC0de@users.noreply.github.com>
2025-10-19 21:50:28 +0200

    fix: DuckDNS throwing a known-after-apply error

--- playground/terraform/dns.tf
+++ playground/terraform/dns.tf
@@ -1,7 +1,7 @@
 # Not exactly clean, since this will run on every `play` already.
 # However, this is a clean way to keep the DNS settings in Terraform.
 data "http" "duckdns-update" {
-  count  = var.dns_name != null && var.duckdns_token != null && local.public_ip != null ? 1 : 0
+  count  = var.dns_name != null && var.duckdns_token != null ? 1 : 0
   url    = "https://www.duckdns.org/update?domains=${var.dns_name}&token=${var.duckdns_token}&ip=${local.public_ip}"
   method = "GET"
 }
